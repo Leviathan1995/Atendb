d9d1428043f252fc199b6a9d295bc773d87b3ce4
Sun Haiqing <Leviathan1995@outlook.com>
2015-03-20 11:05:35 +0800

    update

--- src/Catalog.cpp
+++ src/Catalog.cpp
@@ -1,6 +1,12 @@
 #include "Catalog.h"
 #include "Error.h"
-void Catalog::CatalogCreateTable(string & Tablename, vector<Column_Type> & Attributes)
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>//不加stdlib ofstream报错
+#include "Intepretor.h"
+using namespace std;
+//建立数据表
+void Catalog::CatalogCreateTable(string & Tablename,const vector<Column_Type> & Attributes)
 {
 	Table_Type Tabletc;
 	Tabletc.Table_Name = Tablename;
@@ -21,7 +27,32 @@ void Catalog::CatalogCreateTable(string & Tablename, vector<Column_Type> & Attri
 			NewTableIndex = TableCatalog.size() - 1;
 		}
 	}
+	Column_Type Kc;
+	short KeyIndex = -1, PreviousKeyIndex = 0;
+	for (size_t i = 0; i < Attributes.size(); i++)
+	{
+		Kc.Flag = CATALOG_SPACE_USED;
+		if (Attributes[i].IsPrimary)
+		{
+			Kc.Flag |= CATALOG_IS_PRIMARY_KEY;
+			Kc.Flag |= CATALOG_IS_UNIQUE;
+			Kc.Flag |= CATALOG_IS_NOT_NULL;
+		}
+		if (Attributes[i].IsUnique)
+			Kc.Flag |= CATALOG_IS_UNIQUE;
+		if (Attributes[i].IsNotNull)
+			Kc.Flag |= CATALOG_IS_NOT_NULL;
+		Kc.Column_TypeName = Attributes[i].Column_TypeName;
+		switch (Attributes[i].coltype)
+		{
+		case Int:
+			Kc.coltype = 0;
+		default:
+			break; 
+		}
+	}
 }
+//插入元组
 void Table_Type::InsertColumn(Column_Type column)
 {
 	for (auto i = Table_Column.begin(); i != Table_Column.end(); i++)
@@ -31,13 +62,14 @@ void Table_Type::InsertColumn(Column_Type column)
 	}
 	Table_Column.push_back(column);
 }
+//得到数据表
 Table_Type & Catalog::Get_Table(string table_name)
 {
 	if (Mem_Table.find(table_name) == Mem_Table.end())
 		throw Error();
 	return Mem_Table[table_name];
 }
-
+//得到属性
 static Column_Type & Get_Column(string tablename,string columnname)
 {
 	Table_Type TableInstance;
@@ -47,4 +79,21 @@ static Column_Type & Get_Column(string tablename,string columnname)
 		if (i->Column_TypeName == columnname)
 			return *i;
 	}
+}
+//把数据写入文件
+void Catalog::SaveTable2File()
+{
+	ofstream Fout("catalog/table.dat", ios::binary);
+	Fout.seekp(0, ios::beg);
+	for (size_t i = 0; i < TableCatalog.size(); i++)
+	{
+		Fout.write(&TableCatalog[i].Flag, 1);
+		Fout.write(TableCatalog[i].Table_Name.c_str(), TableCatalog[i].Table_Name.length());
+		Fout.write(&TableCatalog[i].NumberKeys, 1);
+		Fout.write(&TableCatalog[i].PrimaryKey, 1);
+		Fout.write((char *)&TableCatalog[i].indexFlags, sizeof(long));
+		Fout.write((char *)&TableCatalog[i].firstKey, sizeof(short));
+		Fout.write((char *)&TableCatalog[i].firstIndex, sizeof(short));
+	}
+	Fout.close();
 }
--- src/Catalog.h
+++ src/Catalog.h
@@ -15,10 +15,11 @@ class Column_Type
 {
 public:
 	int ID;
+	char Flag;//标志位
 	string Column_TypeName;//字段名
-	ColType coltype;//字段属性
+	char coltype;//字段属性,0 表示 int，1 表示 char(n)，2 表示 float
 	bool IsPrimary;//是不是主键
-	bool IsNull;//是否可以为空
+	bool IsNotNull;//是否可以为空
 	bool IsUnique;//是否为Unique
 	int RequestSize;//用户请求的长度
 	int StoredLength;//实际存储长度
@@ -33,11 +34,16 @@ class Table_Type
 public:
 	Table_Type();//创建数据表
 	char Flag;//标志位
+	char PrimaryKey;//如果>32表示无主键
+	char NumberKeys;//<=32
 	vector<Column_Type> Table_Column;//数据表中的元组存放的地方
 	string Table_Name;//表名
 	int ColumnNum;//数据表具有的属性数目
 	int RecordSize;//记录的长度
 	int PrimaryNum;//多属性主键的个数
+	unsigned long indexFlags; // 对每一位，0 表示该键无索引，1 表示该键有索引
+	unsigned short firstKey; // key 目录文件中，该表第一条键信息的编号
+	short firstIndex; // index 目录文件中，该表第一条索引信息的编号
 	union Table_ptr
 	{
 		Column_Type * Key;//指向主键的指针
@@ -46,23 +52,24 @@ public:
 	void InsertColumn(Column_Type column);//数据表插入属性
 
 };
-class Key_Type
-{
-	char Flag;
-	string KeyName;
-	char KeyType;//键类型，0 int,1 char ,2 float
-	char KeyLength;//键长度
-	short NextKey;//该表下一条键信息的编号，无则为-1
-};
 //数据库中的数据表实例类
 class Catalog
 {
 public :
+	// 标志位相关
 	static const char CATALOG_SPACE_USED = 0x80;
-	void CatalogCreateTable(string & Tablename,vector<Column_Type> & Attributes);
+	static const char CATALOG_HAS_PRIMARY_KEY = 0x40;
+	static const char CATALOG_HAS_INDEX = 0x20;
+	static const char CATALOG_IS_PRIMARY_KEY = 0x10;
+	static const char CATALOG_IS_UNIQUE = 0x08;
+	static const char CATALOG_IS_NOT_NULL = 0x04;
+	static const char CATALOG_IS_INDEX = 0x02;
+	static const char CATALOG_HAS_NEXT = 0x01;
+	void CatalogCreateTable(string & Tablename,const vector<Column_Type> & Attributes);
 	vector<Table_Type> TableCatalog;
 	static map<string, Table_Type> Mem_Table;
 	static Table_Type & Get_Table(string tablename);
 	static Column_Type & Get_Column(string columnname);
+	void SaveTable2File();
 };
 #endif
