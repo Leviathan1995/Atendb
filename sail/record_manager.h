6cbf78e4da6657b2748948034807b1e87b99d1f3
Leviathan <Leviathan1995@outlook.com>
2015-12-10 20:08:58 +0800

    Update record_manager.h

--- sail/record_manager.h
+++ sail/record_manager.h
@@ -1,62 +1,26 @@
 //
-//  record_manager.cpp
+//  record_manager.h
 //  Sail
 //
 //  Created by Leviathan on 15/12/3.
 //  Copyright © 2015年 Leviathan. All rights reserved.
 //
 
-#include "record_manager.h"
+#ifndef record_manager_h
+#define record_manager_h
+
+#include <iostream>
+#include "global.h"
+#include "buffer.h"
 using namespace std;
 
-int record_manager::record_insert(string tablename,table t,record newrecord,buffer *bufferinfo)
+class table;
+//record manager
+class record_manager
 {
-    if(bufferinfo->buffer_fileexist(tablename,RECORD_FILE)==false)
-    {
-        bufferinfo->buffer_createfile(tablename, RECORD_FILE);
-        bufferinfo->buffer_newblock(tablename, RECORD_FILE);
-    }
-    block lastblock=bufferinfo->buffer_readlast(tablename, RECORD_FILE);
-    BYTE * newdata=lastblock.data;
-    int record_length=t.getlength();
-    int record_num=4096/record_length;
-    for (int num=0; num<record_num; num++) {
-        BYTE * record_seek=newdata+num*record_length;
-        if (record_seek[0]==0) {
-            record_seek[0]=1;
-            record_wrecord2block(record_seek,t,newrecord);
-            bufferinfo->buffer_write(lastblock);
-            return lastblock.offset;
-        }
-    }
-    /*
-      don't hava free block
-    */
-    int offset=bufferinfo->buffer_newblock(tablename, RECORD_FILE);
-    block record_block=bufferinfo->buffer_read(tablename,RECORD_FILE, offset);
-    BYTE * record_seek=record_block.data;
-    record_seek[0]=1;
-    record_wrecord2block(record_seek+1, t,newrecord);
-    bufferinfo->buffer_write(record_block);
-    return record_block.offset;
-}
+public:
+    int record_insert(string tablename,table t,record r,buffer * buffer_obj);
+    void record_wrecord2block(BYTE * seek,table t,record r);
+};
 
-void record_manager::record_wrecord2block(BYTE *seek , table t,record newrecord)
-{
-    auto  value=newrecord.records.begin();
-    auto  type=t.columns.begin();
-    for (;value!=newrecord.records.end();value++,type++) {
-        switch (type->type) {
-            case CHAR:
-                memcpy(seek,string2char(value->char_value),value->char_size);
-                seek+=value->char_size;
-                break;
-            case INT:
-                memcpy(seek,&value->int_value, sizeof(int));
-                seek+=sizeof(int);
-                break;
-            default:
-                break;
-        }
-    }
-}
+#endif /* record_manager_h */
