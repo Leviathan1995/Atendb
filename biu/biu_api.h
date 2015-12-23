9108e522688d27114ddd10eeeaae71a1006751e4
Leviathan <Leviathan1995@outlook.com>
2015-12-23 18:13:52 +0800

    Create biu_api.h

---
+++ biu/biu_api.h
@@ -0,0 +1,23 @@
+//
+//  biu.h
+//  Biubitcask
+//
+//  Created by Leviathan on 15/12/23.
+//  Copyright © 2015年 Leviathan. All rights reserved.
+//
+
+#ifndef biu_h
+#define biu_h
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "bitcask.h"
+using namespace std;
+
+string biu_api(string req);
+void exec(bitcask & bit,vector<string> cmd);
+vector<string> intepretor(string req);
+
+
+#endif /* biu_h */
