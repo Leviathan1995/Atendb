be4fd69b0bd04ef64ae9c81b94eb5824c332d3ff
Leviathan <Leviathan1995@outlook.com>
2016-01-05 23:11:53 +0800

    Create biu_comm.h

---
+++ biu/biu_comm.h
@@ -0,0 +1,23 @@
+//
+//  biu_comm.h
+//  Bitcask
+//
+//  Created by Leviathan on 16/1/5.
+//  Copyright © 2016年 Leviathan. All rights reserved.
+//
+
+#ifndef _BIU_COMM_H
+#define _BIU_COMM_H
+
+#include <iostream>
+using namespace std;
+typedef unsigned char byte;
+
+
+int read_cmd(byte *buff);
+int write_cmd(byte *buff,int len);
+int read_exact(byte *buff,int len);
+int write_exact(byte *buff,int len);
+
+
+#endif
