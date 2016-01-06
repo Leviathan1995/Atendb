02761ef9ca1e7ea2641e861fb4f6bfbcc2641857
Leviathan <Leviathan1995@outlook.com>
2016-01-07 01:38:09 +0800

    Update biu_client.erl

--- biu/biu_client.erl
+++ biu/biu_client.erl
@@ -1,15 +1,18 @@
 -module(biu_client).
--export([biu_insert/1]).
+-export([biu_insert/2]).
 
 biu_client() ->
     {ok, Socket} = 
-	gen_tcp:connect("localhost", 2344,
+	gen_tcp:connect("localhost", 1318,
 			[binary, {packet, 4}]),
     Socket.
 
 biu_insert(Key,Value)->
     Socket=biu_client(),
-    ok = gen_tcp:send(Socket, term_to_binary(Str)),
+    ok = gen_tcp:send(Socket, term_to_binary([insert,Key,Value])),
+    response(Socket).
+
+response(Socket)->
     receive
 	{tcp,Socket,Bin} ->
 	    io:format("Client received binary = ~p~n",[Bin]),
