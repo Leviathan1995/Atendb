b83d3c90b2825fd7685a413c2f20c5ccb91c04d0
Leviathan <Leviathan1995@outlook.com>
2016-01-07 01:37:44 +0800

    Update biu_server.erl

--- biu/biu_server.erl
+++ biu/biu_server.erl
@@ -2,7 +2,8 @@
 -export([start_server/0]).
 
 start_server() ->
-	{ok, Listen} = gen_tcp:listen(2344, [binary, {packet, 4},{reuseaddr, true}, {active, true}]),
+	{ok, Listen} = gen_tcp:listen(1318,
+			 [binary, {packet, 4},{reuseaddr, true}, {active, true}]),
 	spawn(fun() -> par_connect(Listen) end).
 
 par_connect(Listen) ->
@@ -12,13 +13,15 @@ par_connect(Listen) ->
 
 loop(Socket) ->
     receive
-	{tcp, Socket, Bin} ->
-	    io:format("Server received binary = ~p~n",[Bin]),
-	    Str = binary_to_term(Bin), 
-	    io:format("Server (unpacked)  ~p~n",[Str]),
-	    Reply =Str, 
-	    io:format("Server replying = ~p~n",[Reply]),
-	    gen_tcp:send(Socket, term_to_binary(Reply)), 
+	{tcp, Socket, Response} ->
+	    Lstr = binary_to_term(Response),
+	    io:format("Server (unpacked)  ~p~n",[Lstr]),
+	    Tstr=list_to_tuple(Lstr),
+	    case Tstr of
+		{insert,Key,Value} ->
+			io:format("shq")
+	    end,
+	    gen_tcp:send(Socket, term_to_binary(Lstr)), 
 	    loop(Socket);
 	{tcp_closed, Socket} ->
 	    io:format("Server socket closed~n")
